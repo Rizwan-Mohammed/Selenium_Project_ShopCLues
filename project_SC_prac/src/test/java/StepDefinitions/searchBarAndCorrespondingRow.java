b3f50c0d872e5a7fad53090ada6536dbcceda044
rizwanmohammed03 <78065694+rizwanmohammed03@users.noreply.github.com>
2024-08-07 23:22:09 -0600

    adding cucumber.junit, cucumber jvm and junit to pom file
    adding search bar and corresponding feature
    
    adding search bar glue code

---
+++ project_SC_prac/src/test/java/StepDefinitions/searchBarAndCorrespondingRow.java
@@ -0,0 +1,32 @@
+package StepDefinitions;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class searchBarAndCorrespondingRow {
+	@Given("when the user is on the homepage of shopclues")
+	public void when_the_user_is_on_the_homepage_of_shopclues() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("the user clicks in search bar and enter the text")
+	public void the_user_clicks_in_search_bar_and_enter_the_text() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@And("user click on search button")
+	public void user_click_on_search_button() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("the user is presented with list options")
+	public void the_user_is_presented_with_list_options() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+}
---
+++ project_SC_prac/src/test/resources/Features/searchBarAndCorrespondingRow.feature
@@ -0,0 +1,12 @@
+# logged by Rizwan
+# On 8th Aug 2024
+
+Feature: Search bar and corresponding row
+
+  Scenario: search bar functionality scenario
+    
+    Given when the user is on the homepage of shopclues 
+    When the user clicks in search bar and enter the text 
+    And user click on search button
+    Then the user is presented with list options
+
